bd69a3c327e097d53a2871f4cbce3ce5527768dc
sonyy789 <sonyy453@naver.com>
2016-02-26 04:35:36 +0000

    permutation by recursion

---
+++ perm.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int perm(int set[], int n, int k, int index)
+{
+    if(index > n)
+    {
+        return 0;
+    }
+    if( k == 0)
+    {
+        for(int i = index; i < n; i++)
+        {
+            set[i] = 0;
+        }
+    
+        for(int i =0; i< n; i++)
+        {
+            cout<<set[i]<<" ";
+        }cout<<endl;
+        
+        return 0;
+    }
+    
+    set[index] = 0;
+    perm(set, n, k, index +1);
+    set[index] = 1;
+    perm(set, n , k - 1 , index + 1 );
+}
+
+int main()
+{
+    int arr[100];
+    int n, k;
+    cin>>n>>k;
+    perm(arr, n, k, 0);
+}
