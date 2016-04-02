b9c36fa1551f2cc8729291cf8b4a191740cb0821
sonyy789 <sonyy453@naver.com>
2016-04-02 05:56:38 +0000

    solved counting sort and sort(algorithm)

--- @@LECTURE.cc
+++ @@LECTURE.cc
@@ -1,64 +1,41 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#define ALPA 26
 using namespace std;
-int sudoku[9][9] = {0};
-int Right[3][3] = {{0,0,1}, {1,0,0}, {0,1,0}};
-
-int arr_init(vector<int> arr, int x, int pointer)
-{
-    if(pointer == 9) return 0;
-    vector<int>::iterator pos;
-    pos = arr.begin() + pointer;
-    for(int i = 0; i < 9; i++)
-    {
-        if(pos == arr.end()) pos = arr.begin();
-        sudoku[i][x] = *pos++;
-    }
-    arr_init(arr, x + 1, pointer + 3);
-}
-int multiple(int x, int y)
-{
-    cout<< x << " "<<y<<endl;
-    int tmp = 0;
-    int cnt = 0;
-    int right_x = x % 3;
-    if( x < 3 ) x = 0;
-    else if( 3 <= x < 6  ) x = 3;
-    else x = 6;
-    
-    for(int i = x; i < x + 3; i++)
-    {
-        tmp += sudoku[i][y] * Right[right_x][cnt++];
-    }
-    return tmp;
-}
-
-int generate(int x, int y)
-{
-   for(int i = y; i < y + 3; i++)
-   {
-       for(int j = x; j < x + 3; j++)
-       {
-           sudoku[j][i+3] = multiple(j,i);
-       }
-   }
-}
+typedef struct NODE{
+    int count = 0;
+    string str;
+} node;
 int main()
 {
-    
-    vector<int> num;
-    for(int i = 0 ; i < 9; i++)
+    int T;
+    cin>>T;
+    while(T--)
     {
-        num.push_back(i+1);
-    }
-    arr_init(num, 0, 0);
-    generate(6,0);
-    for(int i =0 ; i < 9; i++)
-    {
-        for(int j = 0 ; j < 9; j++)
+        string input;
+        cin>>input;
+        node *p = new node[ALPA];
+        for(int i = 0; i < input.size(); i+=2)
         {
-            cout<<sudoku[j][i]<<" ";
+            p[input[i]-97].count++;
+            p[input[i]-97].str.push_back(input[i+1]);
+        }
+        for(int i = 0; i < ALPA; i++)
+        {
+            if(p[i].count) sort(p[i].str.begin(),p[i].str.end());
+        }
+        
+        for(int i = 0; i < ALPA; i++)
+        {
+            if(p[i].count)
+            {
+                for(int j = 0; j < p[i].count; j++)
+                {
+                    cout<<(char)(i+97)<<p[i].str[j];
+                }
+            }
         }cout<<endl;
+        input.clear();
+        delete[] p;
     }
-    cout<<sudoku[6][0]<<endl;
 }
