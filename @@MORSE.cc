9b7e597cbf717b371b8aadf3b2f346724cbe20f8
sonyy789 <sonyy453@naver.com>
2016-05-12 07:03:23 +0000

    Dynamic using by pascal triangle memory

---
+++ @@MORSE.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+#define MAX 1000000000
+using namespace std;
+int mem[201][201];
+void pasc()
+{
+    memset(&mem, 0, sizeof(mem));
+    for(int i = 0; i <= 201; i++)
+    {
+        mem[i][i] = mem[i][0] = 1;
+        for(int j = 1; j < i; j++)
+             mem[i][j] = min(MAX, mem[i-1][j] + mem[i-1][j-1]);
+    }
+}
+void morse(int n, int m, int k)
+{
+    while(n > 0 | m > 0)
+    {
+        int temp = mem[n+m-1][m];
+        if(temp >= k )
+            cout<<"-", n--;
+        else
+            cout<<"o", m--, k -= temp;
+        if(k == 0)
+        {
+            while(m--) cout<<"o";
+            while(n--) cout<<"-";
+        }
+    }
+}
+int main()
+{
+    int T, n, m, k;
+    pasc();
+    cin>>T;
+    while(T--)
+    {
+        cin>>n>>m>>k;
+        morse(n, m, k);
+        cout<<"\n";
+    }
+}
