c1c75f9236b47c8bd01d739b9a1fdf56a109bd25
sonyy789 <sonyy453@naver.com>
2018-03-10 15:26:29 +0000

    it can be solved by union & find >> re commit by version2 later

---
+++ !!10216_bfs_dividegrp_MYSOLUTION.cc
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <queue>
+#include <cmath>
+#include <tuple>
+#include <vector>
+using namespace std;
+vector<tuple<int,int,int>> pos;
+bool dist(int y, int x, int r, int idx){
+	double dist = sqrt(pow(x-get<1>(pos[idx]), 2)+pow(y-get<0>(pos[idx]), 2));
+	return dist<=(double)(r+get<2>(pos[idx]))?true:false;
+}
+int main(){
+	int t, n, cnt, x, y, r;
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d", &n);
+		pos = vector<tuple<int,int,int>>(n);
+		for(int i = 0; i < n; i++){
+			scanf("%d%d%d",&x,&y,&r);
+			pos[i] = make_tuple(x,y,r);
+		}
+		queue<tuple<int,int,int>> q;
+		int ans = 0;
+		while(n){
+			int l = pos.size()-1;
+			ans++;
+			q.push(make_tuple(get<0>(pos[l]),get<1>(pos[l]),get<2>(pos[l])));
+			pos.pop_back(); n--;
+			while(!q.empty()){
+				int yy = get<0>(q.front());
+				int xx = get<1>(q.front());
+				int rr = get<2>(q.front());
+				q.pop();
+				for(int i = pos.size()-1; i>=0; i--){
+					if(dist(yy,xx,rr,i)){
+						q.push(make_tuple(get<0>(pos[i]),get<1>(pos[i]),get<2>(pos[i])));
+						pos.erase(pos.begin()+i); n--;
+					}
+				}
+			}
+		}
+		printf("%d\n", ans);
+	}
+}
