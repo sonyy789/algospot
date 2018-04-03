9b975c6445b954890f1c29f906ec7f8fdbedc94d
sonyy789 <sonyy453@naver.com>
2018-04-03 08:14:46 +0000

    dynamic bottom&up

---
+++ !!11727_TILING2.cc
@@ -0,0 +1,9 @@
+#include <cstdio>
+int n, dp[1001];
+int main(){
+    dp[1] = 1; dp[2] = 3;
+    scanf("%d", &n);
+    for(int i = 3; i <= n; i++)
+        dp[i] = (dp[i-1]+2*dp[i-2])%10007;
+    printf("%d\n", dp[n]);
+}
