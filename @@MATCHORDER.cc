185cc4f6b57ae3228f6c820ed13ce31952d33e7d
sonyy789 <sonyy453@naver.com>
2019-03-28 10:45:21 +0000

    Greedy, O(n)

--- @@MATCHORDER.cc
+++ @@MATCHORDER.cc
@@ -1,43 +1,21 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
 #include <algorithm>
 using namespace std;
-vector<int> ko, ru, stat;
-int matchorder(int N)
-{
-    int ret = 0;
-    for(int i = 0; i < N; i++)
-    {
-        int flags = 0;
-        for(int j = 0; j < N; j++)
-            if(ru[i] <= ko[j] && !stat[j])
-            {
-                stat[j] = 1, ret++;
-                flags = 1;
-                break;
-            }
-        if(flags) continue;
-        for(int j = 0; j < N; j++)
-            if(!stat[j])
-            {
-                stat[j] = 1;
-                break;
-            }
-    }
-    return ret;
-}
-int main()
-{
-    int t, n;
-    cin>>t;
-    while(t--)
-    {
-        cin>>n;
-        ru.clear(), ko.clear();
-        ru = ko =  stat = vector<int>(n, 0);
-        for(int i = 0; i < n; i++) cin>>ru[i];
-        for(int i = 0; i < n; i++) cin>>ko[i];
-        sort(ko.begin(), ko.end());
-        cout<<matchorder(n)<<"\n";
+int c, n, m;
+int A[101], B[101];
+int main(){
+    int i, j;
+    scanf("%d", &c);
+    while(c--){
+        scanf("%d",&n);
+        for(i = 1; i <= n; i++) scanf("%d", &A[i]);
+        for(i = 1; i <= n; i++) scanf("%d", &B[i]);
+        sort(A+1, A+1+n); sort(B+1, B+1+n);
+        i = j = 1;
+        while(i <= n && j <= n){
+            if(A[i]<=B[j]) i++, j++;
+            else j++;
+        }
+        printf("%d\n", i-1);
     }
 }
